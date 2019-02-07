905707a39977ec446208f4312dd2c867a4c6633c
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-07 22:18:52 +0700

    configure MongoDb database

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express()
+const mongoose = require('mongoose');
 
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost/jwtauth');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.listen(PORT, function() {
     console.log('Server is running on PORT', PORT);
 });
+
+app.get('/checking', function(req, res) {
+    res.json({
+        "Tutorial" : "Welcome to the node express JWT Tutorial"
+    });
+})
